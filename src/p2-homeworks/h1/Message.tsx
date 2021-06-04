925cbfcc8140c76d9cf9fa604e4d63183182126c
Biarozka <maksim.biarozka@infourok.biz>
2021-06-04 18:38:07 +0300

    Complete hw1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { FC } from 'react';
 
-function Message() {
-    return (
-        <div>
+import styles from './style.module.scss';
 
-        </div>
-    )
+interface IMessageProps {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+    type: 'GET' | 'POST';
 }
 
-export default Message
+const Message: FC<IMessageProps> = ({ avatar, name, message, time, type }) => {
+    return (
+        <div className={styles['message']}>
+            <div className={styles['message__inner']}>
+                {type === 'GET' && (
+                    <>
+                        <div className={styles['message__img-wrap']}>
+                            <img className={styles['message__img']} src={avatar} alt="" />
+                        </div>
+                        <div
+                            className={`${styles['message__info']} ${styles['message__info--get']}`}>
+                            <p className={styles['message__author']}>{name}</p>
+                            <p className={styles['message__text']}>{message}</p>
+                            <span className={styles['message__time']}>{time}</span>
+                        </div>
+                    </>
+                )}
+                {type === 'POST' && (
+                    <>
+                        <div
+                            className={`${styles['message__info']} ${styles['message__info--post']}`}>
+                            <p className={styles['message__author']}>{name}</p>
+                            <p className={styles['message__text']}>{message}</p>
+                            <span className={styles['message__time']}>{time}</span>
+                        </div>
+                        <div className={styles['message__img-wrap']}>
+                            <img className={styles['message__img']} src={avatar} alt="" />
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Message;
