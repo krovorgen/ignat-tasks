c74983b778e63cc9ea9cded2e6c03f614225a0f6
maksim <maksim.biarozka@infourok.biz>
2021-09-18 16:22:41 +0300

    Added HashRouter

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Routes from './Routes';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 function HW5() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
