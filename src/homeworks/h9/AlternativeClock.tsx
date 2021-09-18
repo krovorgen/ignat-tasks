1b91742c4bea787f4bd5863b75d3d12e3b342263
maksim <maksim.biarozka@infourok.biz>
2021-09-18 19:36:35 +0300

    Test build

---
+++ src/homeworks/h9/AlternativeClock.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AlternativeClock() {
+  return <div></div>;
+}
+
+export default AlternativeClock;
---
+++ src/homeworks/h9/Clock.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import { Button } from '../../components/Button';
+
+function Clock() {
+  const [timerId, setTimerId] = useState<number>(0);
+  const [date, setDate] = useState<Date>();
+  const [show, setShow] = useState<boolean>(false);
+
+  const stop = () => {
+    // stop
+  };
+  const start = () => {
+    stop();
+    const id: number = window.setInterval(() => {
+      // setDate
+    }, 1000);
+    setTimerId(id);
+  };
+
+  const onMouseEnter = () => {
+    // show
+  };
+  const onMouseLeave = () => {
+    // close
+  };
+
+  const stringTime = 'Time'; // fix with date
+  const stringDate = 'Date'; // fix with date
+
+  return (
+    <div>
+      <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        {stringTime}
+      </div>
+
+      {show && <div>{stringDate}</div>}
+
+      <Button onClick={start}>start</Button>
+      <Button onClick={stop}>stop</Button>
+    </div>
+  );
+}
+
+export default Clock;
